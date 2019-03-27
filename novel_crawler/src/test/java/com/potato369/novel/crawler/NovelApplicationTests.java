a331a22accd6edb345d029fcd62b19c7ffa4adb6
wangyanjun <406151651@qq.com>
2019-03-27 10:05:54 +0800

    修改测试类的名字。

--- novel_crawler/src/test/java/com/potato369/novel/crawler/NovelApplicationTests.java
+++ novel_crawler/src/test/java/com/potato369/novel/crawler/NovelApplicationTests.java
@@ -1,4 +1,4 @@
-package com.potato369.novel.app.web;
+package com.potato369.novel.crawler;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class NovelApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
