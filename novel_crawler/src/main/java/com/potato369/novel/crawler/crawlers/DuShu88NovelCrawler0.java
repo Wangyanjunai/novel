c584890688fe5bd4c9677caeb709a23b8f3df4f0
wangyanjun <406151651@qq.com>
2019-05-06 23:10:00 +0800

    修改爬虫系统代码,添加脚本文件。

--- novel_crawler/src/main/java/com/potato369/novel/crawler/crawlers/DuShu88NovelCrawler0.java
+++ novel_crawler/src/main/java/com/potato369/novel/crawler/crawlers/DuShu88NovelCrawler0.java
@@ -37,12 +37,12 @@ public class DuShu88NovelCrawler0 extends BaseSeimiCrawler{
 
 	@Autowired
 	private NovelInfoService novelInfoService;
-	
+
 	@Override
 	public String[] startUrls() {
 		return new String[]{DOMAIN_URL};
 	}
-	
+
 	@Override
 	public void start(Response response) {
         try {
@@ -351,6 +351,7 @@ public class DuShu88NovelCrawler0 extends BaseSeimiCrawler{
 			}
 		}
 	}
+
     public static void main(String[] args) {
     	NovelInfo source = NovelInfo.builder().build();
     	source.setId("0057cd19e7ee45c4872c686b5425d697");
@@ -398,58 +399,62 @@ public class DuShu88NovelCrawler0 extends BaseSeimiCrawler{
     	NovelInfo novelInfo = copy(source, target);
     	log.info("novel info copy={}", novelInfo);
 	}
-	private static NovelInfo copy(NovelInfo source, NovelInfo target) {
+
+	public static NovelInfo copy(NovelInfo source, NovelInfo target) {
 		if (source != null && target != null) {
 			if (StringUtils.isNotEmpty(source.getId())) {
-				target.setId(source.getId());
+				target.setId(source.getId());//设置小说id
 			}
 			if (StringUtils.isNotEmpty(source.getCoverURL())) {
-				target.setCoverURL(source.getCoverURL());
+				target.setCoverURL(source.getCoverURL());//设置小说封面图片路径url
 			}
 			if (source.getCategoryType()!= null) {
-				target.setCategoryType(source.getCategoryType());
+				target.setCategoryType(source.getCategoryType());//设置小说分类类型type
 			}
 			if (StringUtils.isNotEmpty(source.getCategoryENText())) {
-				target.setCategoryENText(source.getCategoryENText());
+				target.setCategoryENText(source.getCategoryENText());//设置小说分类英文名称
 			}
 			if (StringUtils.isNotEmpty(source.getCategoryCNText())) {
-				target.setCategoryCNText(source.getCategoryCNText());
+				target.setCategoryCNText(source.getCategoryCNText());//设置小说分类中文名称
 			}
 			if (StringUtils.isNotEmpty(source.getAuthor())) {
-				target.setAuthor(source.getAuthor());
+				target.setAuthor(source.getAuthor());//设置小说作者笔名
 			}
 			if (StringUtils.isNotEmpty(source.getTitle())) {
-				target.setTitle(source.getTitle());
+				target.setTitle(source.getTitle());//设置小说标题（名称）
 			}
 			if (StringUtils.isNotEmpty(source.getIntroduction())) {
-				target.setIntroduction(source.getIntroduction());
+				target.setIntroduction(source.getIntroduction());//设置小说简介
 			}
 			if (StringUtils.isNotEmpty(source.getNewestChapterId())) {
-				target.setNewestChapterId(source.getNewestChapterId());
+				target.setNewestChapterId(source.getNewestChapterId());//设置小说最新章节id
 			}
 			if (StringUtils.isNotEmpty(source.getNewestChapterTitle())) {
-				target.setNewestChapterTitle(source.getNewestChapterTitle());
+				target.setNewestChapterTitle(source.getNewestChapterTitle());//设置小说最新章节标题
 			}
 			if (source.getRetention() != null && !Integer.valueOf(0).equals(source.getRetention())) {//留存率，现在只是保存数字，显示的时候加上百分比
-				target.setRetention(source.getRetention());
+				target.setRetention(source.getRetention());//设置小说留存率，现在只是保存数字，显示的时候加上百分比
 			}
 			if (source.getTotalChapters() != null && !Integer.valueOf(0).equals(source.getTotalChapters())) {//章节总数
-				target.setTotalChapters(source.getTotalChapters());
+				target.setTotalChapters(source.getTotalChapters());//设置小说总章节数
 			}
 			if (source.getClickNumber() != null && !BigDecimal.ZERO.equals(source.getClickNumber())) {//点击次数
-				target.setClickNumber(source.getClickNumber());
+				target.setClickNumber(source.getClickNumber());//设置小说点击次数
 			}
 			if (source.getReaders() != null && !BigDecimal.ZERO.equals(source.getReaders())) {//阅读（点击）用户数；默认“0-阅读（点击）用户数”
-				target.setReaders(source.getReaders());
+				target.setReaders(source.getReaders());//设置小说阅读（点击）用户数；默认“0-阅读（点击）用户数”
 			}
 			if (source.getRecentReaders()!=null &&  !BigDecimal.ZERO.equals(source.getRecentReaders())) {//最近跟随阅读（点击）用户数；默认“0-最近跟随阅读（点击）用户数”
-				target.setRecentReaders(source.getRecentReaders());
+				target.setRecentReaders(source.getRecentReaders());//设置小说最近跟随阅读（点击）用户数；默认“0-最近跟随阅读（点击）用户数”
 			}
 			if (source.getTotalWords() != null && !BigDecimal.ZERO.equals(source.getTotalWords())) {//总字数
-				target.setTotalWords(source.getTotalWords());
+				target.setTotalWords(source.getTotalWords());//设置小说总字数
 			}
 			if (source.getCreateTime() != null) {
-				target.setCreateTime(source.getCreateTime());
+				target.setCreateTime(source.getCreateTime());//设置小说创建时间
+			}
+			if (source.getUpdateTime() != null) {
+				target.setUpdateTime(source.getUpdateTime());//设置小说更新时间
 			}
 		}
 		return target;
