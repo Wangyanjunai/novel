f9eda1b895caf4d3131f529c1c2bb9de14499f99
wangyanjun <406151651@qq.com>
2019-07-01 17:22:06 +0800

    优化打包代码。

--- novel_app_web/src/main/java/com/potato369/novel/app/web/NovelApplication.java
+++ novel_app_web/src/main/java/com/potato369/novel/app/web/NovelApplication.java
@@ -4,17 +4,26 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
 @ComponentScan(basePackages = {"com.potato369.novel.basic.service", "com.potato369.novel.app.web", "com.potato369.novel.app.web.controller"})
+@EnableCaching
 @EnableJpaRepositories(basePackages = "com.potato369.novel.basic.repository")
 @EntityScan(basePackages = "com.potato369.novel.basic.dataobject")
 @MapperScan(basePackages = "com.potato369.novel.basic.repository.mapper")
-public class NovelApplication {
+@SpringBootApplication
+public class NovelApplication extends SpringBootServletInitializer{
     public static void main(String[] args) {
         System.setProperty("https.protocols", "TLSv1.2");
         SpringApplication.run(NovelApplication.class, args);
     }
+    
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 }
